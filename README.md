30e65089dc50d7baba684f1182d7e6a25d9f52c8
unknown <dilumnavanjana@gmail.com>
2015-11-27 08:46:05 +0530

    custom directives added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a repository with sample AngularJS code that I'm using for my testing purposes.
+
+Anyone can use those samples without any permission.
--- dynamic.js
+++ dynamic.js
@@ -3,55 +3,68 @@ mainApp.controller('DynamicController', ['$scope', function($scope){
   var users = [
       { 
         name: 'Lalin',
+        type :'text',
         email: '' ,
         isText: true
       },
       {
-        name: 'Holly',
+        name: 'Gihan',
+        type :'checkbox',
         email: 'asdfasd@gmail.com' ,
       },
       {
         name: 'John',
+        type :'checkbox',
         email: 'John@gmail.com' ,
       },
       {
         name: 'Dilum',
+        type :'text',
         email: 'Dilum@gmail.com' ,
       },
       {
         name: 'Navanjana',
+        type :'radio',
         email: 'Navanjana@gmail.com' ,
       },
       {
         name: 'Wikum',
+        type :'radio',
         email: 'wikum@gmail.com' ,
       },
       {
         name: 'Gayan',
+        type :'week',
         email: 'Gayan@gmail.com' ,
       },
       {
         name: 'Dhanika',
+        type :'week',
         email: 'Dhanika@gmail.com' ,
       },
       {
         name: 'Dishan',
+        type :'range',
         email: 'Dishan@gmail.com' ,
       },
       {
         name: 'Prasad',
+        type :'range',
         email: 'Prasad@gmail.com' ,
       },
       {
         name: 'Menu',
+        type :'text',
         email: 'Menu@gmail.com' ,
       },
       {
         name: 'Lahiru',
+        type :'text',
         email: 'Lahiru@gmail.com' ,
       },
       {
         name: 'Biman',
+        type :'text',
         email: 'Biman@gmail.com' ,
       }
   ];
@@ -69,4 +82,38 @@ mainApp.controller('DynamicController', ['$scope', function($scope){
 
   $scope.formData = {};
   $scope.formData.users = users;
-}]);
+}])
+
+.directive('dynamicView', function() {
+    return {
+      templateUrl: 'dynamic-view.html'
+    };
+});
+
+// mainApp.directive('input', function() {
+//     return {
+//         restrict: 'E',
+//         require: 'ngModel',
+//         link: function (scope, element, attr, ngModel) {
+//           if (attr.type !== 'path') return;
+
+//           // Override the input event and add custom 'path' logic
+//           // element.unbind('input');
+//           // element.bind('input', function () {
+//           //   var path = this.value.replace(/\\/g, '/');
+
+//           //   scope.$apply(function () {
+//           //     ngModel.$setViewValue(path);
+//           //   });
+//           // });
+
+//           attr.type = "checkbox";
+//         }
+//     };
+// });
+
+// mainApp.directive('dynamicView', function() {
+//     return {
+//       templateUrl: 'dynamic-view.html'
+//     };
+// });
