2936575783d2d7fcfb99e298c9f3f9038873733b
unknown <dilumnavanjana@gmail.com>
2015-12-15 16:47:21 +0530

    latest json with navigation

--- sample.js
+++ sample.js
@@ -1,7 +1,343 @@
+/*
+Factory implementation using 'dataAccessService' to provide data access to 'Emergency Contacts' controller
+*/
 (function (angular) {
     'use strict';
 
     var formdataService = function () {
+
+        var jsonResponse2 = {
+            "formID": "1",
+            "formName": "Form1",
+            "sections": [
+{
+    "sectionID": "1",
+    "sectionName": "Section 1",
+    "questions":
+[
+  {
+      "sectionID": "1",
+      "questionID": "1",
+      "questionName": "Section1 Question1",
+      "type": "text",
+      "content": "Textcontent",
+      "value": "65",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": "",
+      "workflowSender": {
+          "sectionID": "1",
+          "questionID": "4",
+          "logic": "Textcon",
+          "category": "showHide"
+      }
+  },
+  {
+      "sectionID": "1",
+      "questionID": "2",
+      "questionName": "Section1 Question2",
+      "type": "text",
+      "content": "Dilum Content",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": "",
+      "workflowSender": {
+          "sectionID": "2",
+          "questionID": "4",
+          "logic": "con",
+          "category": "sectionNavigation"
+      }
+  },
+{
+    "sectionID": "1",
+    "questionID": "3",
+    "questionName": "Asking Question Here",
+    "type": "text",
+    "content": "text content here",
+    "value": "65",
+    "isAssesment": true,
+    "isAssesmentCheck": true,
+    "assesmentDate": ""
+},
+  {
+      "sectionID": "1",
+      "questionID": "4",
+      "questionName": "What is your",
+      "type": "text",
+      "content": "Height??",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": "",
+      "workflowReceiver": {
+          "senderSectionID": "1",
+          "senderQuestionID": "1",
+          "category": "showHide",
+          "show": false
+      }
+  }]
+},
+{
+    "sectionID": "2",
+    "sectionName": "Section 2",
+    "questions":
+[
+  {
+      "sectionID": "2",
+      "questionID": "1",
+      "questionName": "Please select one",
+      "type": "radio",
+      "content": [
+                 {
+                     "name": "Yes",
+                     "isChecked": false
+                 },
+                 {
+                     "name": "No",
+                     "isChecked": true
+                 },
+                 {
+                     "name": "Nothing",
+                     "isChecked": false
+                 },
+                 {
+                     "name": "Last One",
+                     "isChecked": false
+                 }
+
+      ],
+      "value": "65",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "2",
+      "questionID": "2",
+      "questionName": "Checkbox here",
+      "type": "checkbox",
+      "content": false,
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "2",
+      "questionID": "3",
+      "questionName": "Body weight",
+      "type": "text",
+      "content": "textbox content",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": "",
+      "workflowSender": {
+          "sectionID": "2",
+          "questionID": "4",
+          "logic": "show"
+      }
+  }
+  ,
+  {
+      "sectionID": "2",
+      "questionID": "4",
+      "questionName": "Marks",
+      "type": "text",
+      "content": "324",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": "",
+      "workflowReceiver": {
+          "senderSectionID": "2",
+          "senderQuestionID": "3",
+          "logic": "showHide",
+          "show": false
+      }
+  },
+  {
+      "sectionID": "2",
+      "questionID": "5",
+      "questionName": "Mothers name",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "2",
+      "questionID": "6",
+      "questionName": "Fathers name",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "2",
+      "questionID": "7",
+      "questionName": "Brother Name",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "2",
+      "questionID": "8",
+      "questionName": "Sister Name",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  }
+]
+},
+{
+    "sectionID": "3",
+    "sectionName": "Section 3",
+    "questions":
+[
+  {
+      "sectionID": "3",
+      "questionID": "1",
+      "questionName": "Section3 Question1",
+      "type": "textarea",
+      "content": "Textarea content here",
+      "value": "65",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "2",
+      "questionName": "Section3 Question2",
+      "type": "text",
+      "content": "Dilum Content",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "3",
+      "questionName": "Question3 content",
+      "type": "text",
+      "content": "Content",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "4",
+      "questionName": "Why are you leaving?",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "5",
+      "questionName": "Reason??",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "6",
+      "questionName": "Home Town",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "7",
+      "questionName": "Mobile Number",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "8",
+      "questionName": "Land Number",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "9",
+      "questionName": "Address",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "10",
+      "questionName": "Home Address",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  },
+  {
+      "sectionID": "3",
+      "questionID": "11",
+      "questionName": "Fax",
+      "type": "text",
+      "content": "",
+      "value": "55",
+      "isAssesment": true,
+      "isAssesmentCheck": true,
+      "assesmentDate": ""
+  }
+
+]
+}
+            ]
+        };
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////Layout Json///////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
 
         var layoutRespnse = {
             "formID": "1",
@@ -17,7 +353,7 @@
   },
   {
       "questionID": "2",
-      "type": "textarea",
+      "type": "text",
       "isAssesment": true
   },
 {
@@ -166,10 +502,13 @@
       "value": "65",
       "isAssesmentCheck": true,
       "assesmentDate": "",
-      "workflowSender": {
-          "sectionID": "1",
-          "questionID": "4",
-          "logic": "Textcon"
+      "workflow": {
+          "category": "showHide",
+          "workflowSender": {
+              "sectionID": "1",
+              "questionID": "4",
+              "logic": "Textcon"
+          }
       }
   },
   {
@@ -178,8 +517,22 @@
       "questionName": "Section1 Question2",
       "content": "Dilum Content",
       "value": "55",
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
+      "isAssesmentCheck": false,
+      "assesmentDate": "",
+      "workflow": {
+          "category": "sectionNavigation",
+          "sectionID": "2",
+          "workflowSender": {
+              "sectionID": "1",
+              "questionID": "2",
+              "logic": "test"
+          },
+          "workflowReceiver": {
+              "senderSectionID": "1",
+              "senderQuestionID": "2",
+              "show": false
+          }
+      }
   },
 {
     "sectionID": "1",
@@ -187,7 +540,7 @@
     "questionName": "Asking Question Here",
     "content": "text content here",
     "value": "65",
-    "isAssesmentCheck": true,
+    "isAssesmentCheck": false,
     "assesmentDate": ""
 },
   {
@@ -196,13 +549,15 @@
       "questionName": "What is your height",
       "content": "Height??",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": "",
-      "workflowReceiver": {
-          "senderSectionID": "1",
-          "senderQuestionID": "1",
-          "logic": "showHide",
-          "show": true
+      "workflow": {
+          "category": "showHide",
+          "workflowReceiver": {
+              "senderSectionID": "1",
+              "senderQuestionID": "1",
+              "show": false
+          }
       }
   }]
 },
@@ -235,7 +590,7 @@
 
       ],
       "value": "65",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -244,7 +599,7 @@
       "questionName": "Checkbox here",
       "content": true,
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   }
   ,
@@ -264,7 +619,7 @@
       "questionName": "Marks",
       "content": "324",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -282,7 +637,7 @@
       "questionName": "Fathers name",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -291,7 +646,7 @@
       "questionName": "Brother Name",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -300,7 +655,7 @@
       "questionName": "Sister Name",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   }
 ]
@@ -316,7 +671,7 @@
       "questionName": "Section3 Question1",
       "content": "Textarea content here",
       "value": "65",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -334,7 +689,7 @@
       "questionName": "Question3 content",
       "content": "Content",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -343,7 +698,7 @@
       "questionName": "Why are you leaving?",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -352,7 +707,7 @@
       "questionName": "Reason??",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -361,7 +716,7 @@
       "questionName": "Home Town",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -370,7 +725,7 @@
       "questionName": "Mobile Number",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -379,7 +734,7 @@
       "questionName": "Land Number",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -388,7 +743,7 @@
       "questionName": "Address",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -397,7 +752,7 @@
       "questionName": "Home Address",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   },
   {
@@ -406,7 +761,7 @@
       "questionName": "Fax",
       "content": "",
       "value": "55",
-      "isAssesmentCheck": true,
+      "isAssesmentCheck": false,
       "assesmentDate": ""
   }
 
