8512bbee11d522ebc7cb72788aca3c2d4c3f7de6
unknown <dilumnavanjana@gmail.com>
2015-12-22 08:48:52 +0530

    remove oldest model from json

--- sample.js
+++ sample.js
@@ -6,335 +6,6 @@ Factory implementation using 'dataAccessService' to provide data access to 'Emer
 
     var formdataService = function () {
 
-        var jsonResponse2 = {
-            "formID": "1",
-            "formName": "Form1",
-            "sections": [
-{
-    "sectionID": "1",
-    "sectionName": "Section 1",
-    "questions":
-[
-  {
-      "sectionID": "1",
-      "questionID": "1",
-      "questionName": "Section1 Question1",
-      "type": "text",
-      "content": "Textcontent",
-      "value": "65",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": "",
-      "workflowSender": {
-          "sectionID": "1",
-          "questionID": "4",
-          "logic": "Textcon",
-          "category": "showHide"
-      }
-  },
-  {
-      "sectionID": "1",
-      "questionID": "2",
-      "questionName": "Section1 Question2",
-      "type": "text",
-      "content": "Dilum Content",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": "",
-      "workflowSender": {
-          "sectionID": "2",
-          "questionID": "4",
-          "logic": "con",
-          "category": "sectionNavigation"
-      }
-  },
-{
-    "sectionID": "1",
-    "questionID": "3",
-    "questionName": "Asking Question Here",
-    "type": "text",
-    "content": "text content here",
-    "value": "65",
-    "isAssesment": true,
-    "isAssesmentCheck": true,
-    "assesmentDate": ""
-},
-  {
-      "sectionID": "1",
-      "questionID": "4",
-      "questionName": "What is your",
-      "type": "text",
-      "content": "Height??",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": "",
-      "workflowReceiver": {
-          "senderSectionID": "1",
-          "senderQuestionID": "1",
-          "category": "showHide",
-          "show": false
-      }
-  }]
-},
-{
-    "sectionID": "2",
-    "sectionName": "Section 2",
-    "questions":
-[
-  {
-      "sectionID": "2",
-      "questionID": "1",
-      "questionName": "Please select one",
-      "type": "radio",
-      "content": [
-                 {
-                     "name": "Yes",
-                     "isChecked": false
-                 },
-                 {
-                     "name": "No",
-                     "isChecked": true
-                 },
-                 {
-                     "name": "Nothing",
-                     "isChecked": false
-                 },
-                 {
-                     "name": "Last One",
-                     "isChecked": false
-                 }
-
-      ],
-      "value": "65",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "2",
-      "questionID": "2",
-      "questionName": "Checkbox here",
-      "type": "checkbox",
-      "content": false,
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "2",
-      "questionID": "3",
-      "questionName": "Body weight",
-      "type": "text",
-      "content": "textbox content",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": "",
-      "workflowSender": {
-          "sectionID": "2",
-          "questionID": "4",
-          "logic": "show"
-      }
-  }
-  ,
-  {
-      "sectionID": "2",
-      "questionID": "4",
-      "questionName": "Marks",
-      "type": "text",
-      "content": "324",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": "",
-      "workflowReceiver": {
-          "senderSectionID": "2",
-          "senderQuestionID": "3",
-          "logic": "showHide",
-          "show": false
-      }
-  },
-  {
-      "sectionID": "2",
-      "questionID": "5",
-      "questionName": "Mothers name",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "2",
-      "questionID": "6",
-      "questionName": "Fathers name",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "2",
-      "questionID": "7",
-      "questionName": "Brother Name",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "2",
-      "questionID": "8",
-      "questionName": "Sister Name",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  }
-]
-},
-{
-    "sectionID": "3",
-    "sectionName": "Section 3",
-    "questions":
-[
-  {
-      "sectionID": "3",
-      "questionID": "1",
-      "questionName": "Section3 Question1",
-      "type": "textarea",
-      "content": "Textarea content here",
-      "value": "65",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "2",
-      "questionName": "Section3 Question2",
-      "type": "text",
-      "content": "Dilum Content",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "3",
-      "questionName": "Question3 content",
-      "type": "text",
-      "content": "Content",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "4",
-      "questionName": "Why are you leaving?",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "5",
-      "questionName": "Reason??",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "6",
-      "questionName": "Home Town",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "7",
-      "questionName": "Mobile Number",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "8",
-      "questionName": "Land Number",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "9",
-      "questionName": "Address",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "10",
-      "questionName": "Home Address",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  },
-  {
-      "sectionID": "3",
-      "questionID": "11",
-      "questionName": "Fax",
-      "type": "text",
-      "content": "",
-      "value": "55",
-      "isAssesment": true,
-      "isAssesmentCheck": true,
-      "assesmentDate": ""
-  }
-
-]
-}
-            ]
-        };
-
         //////////////////////////////////////////////////////////////////////////////////////////////////////
         ////////////////////////////////////Layout Json///////////////////////////////////////////////////////
         //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -358,7 +29,7 @@ Factory implementation using 'dataAccessService' to provide data access to 'Emer
   },
 {
     "questionID": "3",
-    "type": "text",
+    "type": "image",
     "isAssesment": true
 },
   {
@@ -390,7 +61,7 @@ Factory implementation using 'dataAccessService' to provide data access to 'Emer
   ,
   {
       "questionID": "4",
-      "type": "text",
+      "type": "signature",
       "isAssesment": true
   },
   {
@@ -515,8 +186,8 @@ Factory implementation using 'dataAccessService' to provide data access to 'Emer
 {
     "sectionID": "1",
     "questionID": "3",
-    "questionName": "Asking Question Here",
-    "content": "text content here",
+    "questionName": "Showing Image Here",
+    "content": "http://www.bestmotherofthegroomspeeches.com/wp-content/themes/thesis/rotator/sample-1.jpg",
     "value": "65",
     "isAssesmentCheck": false,
     "assesmentDate": ""
@@ -913,12 +584,6 @@ Factory implementation using 'dataAccessService' to provide data access to 'Emer
             ]
         };
 
-
-
-        var getResponseData = function () {
-            return jsonResponse2;
-        };
-
         var getLayoutData = function () {
             return layoutRespnse;
         };
@@ -933,7 +598,6 @@ Factory implementation using 'dataAccessService' to provide data access to 'Emer
 
 
         var service = {
-            getResponseData: getResponseData,
             getLayoutData: getLayoutData,
             getSectionData: getSectionData,
             getWorkflowData: getWorkflowData
